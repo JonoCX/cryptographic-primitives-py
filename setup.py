eb85e735e7e86c1e9aa444b6ed0b1d94741ed248
JonoCX <j.carlton@newcastle.ac.uk>
2015-10-28 22:53:16 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cryptographic-primitives-py',
+    version='0.1',
+    packages=['tests', 'crypto-primitives'],
+    url='',
+    license='',
+    author='Jonathan Carlton',
+    author_email='jonathan.carlton@hotmail.com',
+    description='A selection of cryptographic functions'
+)
---
+++ tests/crypto-primitives-tests.py
@@ -0,0 +1,13 @@
+__author__ = 'Jonathan'
+
+
+def setup():
+    return None
+
+
+def teardown():
+    return None
+
+
+def test_1():
+    return None
